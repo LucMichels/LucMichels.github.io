b90f4dcfc9bad95ac5cb88fc778b7d24e6d2999a
Luc Michels <3gmichels@gmail.com>
2018-10-07 12:02:29 +0900

    test

---
+++ software/map.js
@@ -0,0 +1,14 @@
+var img = $('img')
+                          
+img.mapster({
+    mapKey: 'state',
+    fill: true,
+    fillColor: '00FF00',
+    border: true,
+    stroke: true,
+    staticState: true
+})
+
+//.bind('mouseover',function() {
+//	img.set('set',"1",false).set('set',true,"1",{ fillColor: 'ff0000' })
+//});
