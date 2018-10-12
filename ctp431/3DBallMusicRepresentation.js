d576a62396f847a7fc393cbe5a458b75d88e4fad
Luc Michels <3gmichels@gmail.com>
2018-10-12 11:33:24 +0900

    hurdur

--- ctp431/3DBallMusicRepresentation.js
+++ ctp431/3DBallMusicRepresentation.js
@@ -166,7 +166,7 @@ function addLights(){
 		var randomDist = minLightDist + Math.random()*lightDist //for point light
 		var randomDirection = p5.Vector.random3D().normalize()
 		print(randomDist+sphereRadius)
-		var position = randomDirection*mult(randomDist+sphereRadius) //for point light
+		var position = randomDirection.mult(randomDist+sphereRadius) //for point light
 		
 		var randomColor = color(round(Math.random()*255), round(Math.random()*255), round(Math.random()*255))
 
