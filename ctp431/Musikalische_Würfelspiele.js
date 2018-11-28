a25f14a814bc1f15e1806ee903a321e7eb5e1886
LucLeGros <3gmichels@gmail.com>
2018-11-28 13:09:38 +0900

    eh

--- ctp431/Musikalische_Würfelspiele.js
+++ ctp431/Musikalische_Würfelspiele.js
@@ -1,29 +1,14 @@
 var player = MIDI.player
 
 window.onload = function () {
-	MIDI.loadPlugin({
-		soundfontUrl: "https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/",
-		instrument: "acoustic_grand_piano",
-		onprogress: function(state, progress) {
-			console.log(state, progress);
-		},
-		onsuccess: function() {
-
-			player = MIDI.player;
-			player.timeWarp = 1; // speed the song is played back
-			player.loadFile("assets/0/cda010.mid", player.start);
-			//player.getNextSong = function(n) {
-				
-			//	player.loadFile("assets/0/cda010.mid", player.start); // load MIDI
-			//};
-			MIDI.Player.setAnimation(function(data) {
-    			var now = data.now; // where we are now
-    			var end = data.end; // time when song ends
-    			if(now == end){
-    				player.loadFile("assets/0/cda070.mid", player.start);
-    			}
-    			
-			});
-		}
+	var MidiPlayer = require('midi-player-js');
+	 
+	// Initialize player and register event handler
+	var Player = new MidiPlayer.Player(function(event) {
+	    console.log(event);
 	});
+	 
+	// Load a MIDI file
+	Player.loadFile('./test.mid');
+	Player.play();
 };
