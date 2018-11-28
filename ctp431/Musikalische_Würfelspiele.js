dc168c68b3d95942713893bc1945e91220c8200f
LucLeGros <3gmichels@gmail.com>
2018-11-28 13:31:39 +0900

    ùmhhh

--- ctp431/Musikalische_Würfelspiele.js
+++ ctp431/Musikalische_Würfelspiele.js
@@ -1,7 +1,7 @@
 
 
 window.onload = function () {
-	var MidiPlayer = require('[midiplayer.min]');
+	var MidiPlayer = require(['midiplayer.min']);
 	 
 	// Initialize player and register event handler
 	var Player = new MidiPlayer.Player(function(event) {
