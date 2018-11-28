cdc9d4224086cf0124a5959f36e1ba5a424d0105
LucLeGros <3gmichels@gmail.com>
2018-11-28 12:12:12 +0900

    testing

---
+++ ctp431/Musikalische_Würfelspiele.js
@@ -0,0 +1,29 @@
+var player = MIDI.player
+
+window.onload = function () {
+	MIDI.loadPlugin({
+		soundfontUrl: "./soundfont/",
+		instrument: "acoustic_grand_piano",
+		onprogress: function(state, progress) {
+			console.log(state, progress);
+		},
+		onsuccess: function() {
+
+			player = MIDI.player;
+			player.timeWarp = 1; // speed the song is played back
+			player.loadFile("assets/0/cda010.mid", player.start);
+			//player.getNextSong = function(n) {
+				
+			//	player.loadFile("assets/0/cda010.mid", player.start); // load MIDI
+			//};
+			MIDI.Player.setAnimation(function(data) {
+    			var now = data.now; // where we are now
+    			var end = data.end; // time when song ends
+    			if(now == end){
+    				player.loadFile("assets/0/cda070.mid", player.start);
+    			}
+    			
+			});
+		}
+	});
+};
