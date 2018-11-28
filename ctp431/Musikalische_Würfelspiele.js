1804e9e26bb688ad08f3314b16f217ffa7daf152
LucLeGros <3gmichels@gmail.com>
2018-11-28 16:05:05 +0900

    maybe

--- ctp431/Musikalische_Würfelspiele.js
+++ ctp431/Musikalische_Würfelspiele.js
@@ -1,14 +1,26 @@
 
 
 window.onload = function () {
-	var fs = require('fs');
-	 
-	// Initialize player and register event handler
-	var Player = new MidiPlayer.Player(function(event) {
-	    console.log(event);
-	});
-	 
-	// Load a MIDI file
-	Player.loadFile('./assets/0/cda070.mid');
-	Player.play();
+	// load a midi file
+	MidiConvert.load("assets/0/cda010.mid", function(midi) {
+	  console.log(midi)
+	})
+	var synth = new Tone.PolySynth(8).toMaster()
+
+	MidiConvert.load("assets/0/cda070.mid", function(midi) {
+
+	  // make sure you set the tempo before you schedule the events
+	  Tone.Transport.bpm.value = midi.header.bpm
+
+	  // pass in the note events from one of the tracks as the second argument to Tone.Part 
+	  var midiPart = new Tone.Part(function(time, note) {
+
+	    //use the events to play the synth
+	    synth.triggerAttackRelease(note.name, note.duration, time, note.velocity)
+
+	  }, midi.tracks[0].notes).start()
+
+	  // start the transport to hear the events
+	  Tone.Transport.start()
+	})
 };
