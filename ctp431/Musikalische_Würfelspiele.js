e7f63b0b02ab3a9bb5b87052bf5ea090447c62c1
LucLeGros <3gmichels@gmail.com>
2018-11-28 18:29:53 +0900

    play

--- ctp431/Musikalische_Würfelspiele.js
+++ ctp431/Musikalische_Würfelspiele.js
@@ -41,20 +41,5 @@ function playMusic(){
 	})
 }
 function playMP3(){
-	MidiConvert.load("assets/0/cda070.mid", function(midi) {
-		
-	  // make sure you set the tempo before you schedule the events
-	  Tone.Transport.bpm.value = midi.header.bpm
-	  
-	  // pass in the note events from one of the tracks as the second argument to Tone.Part 
-	  var midiPart = new Tone.Part(function(time, note) {
-
-	    //use the events to play the synth
-	    piano.triggerAttackRelease(note.name, note.duration, time, note.velocity)
-
-	  }, midi.tracks[0].notes).start()
-
-	  // start the transport to hear the events
-	  Tone.Transport.start()
-	})
+	player.play()
 }
