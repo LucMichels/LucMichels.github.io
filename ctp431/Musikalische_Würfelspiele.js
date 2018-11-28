466a213cd63692f2b77c4939e52d0b205ae3b797
LucLeGros <3gmichels@gmail.com>
2018-11-28 18:26:09 +0900

    test

--- ctp431/Musikalische_Würfelspiele.js
+++ ctp431/Musikalische_Würfelspiele.js
@@ -1,5 +1,7 @@
 
-
+var piano
+var synth
+var player
 window.onload = function () {
 	// load a midi file
 	MidiConvert.load("assets/0/cda010.mid", function(midi) {
@@ -8,16 +10,37 @@ window.onload = function () {
 	  console.log("intru " +midi.instrument)
 
 	})
-	var synth = new Tone.PolySynth(8).toMaster()
-	var piano = SampleLibrary.load({
+	synth = new Tone.PolySynth(8).toMaster()
+	piano = SampleLibrary.load({
 		  instruments: "piano"
 		  });
 		  
 	piano.toMaster();
+	player = new Tone.Player("./assets/0/cda070.mp3", function(){
+	//the player is now ready	
+	}).toMaster();
 	
 };
 
 function playMusic(){
+	MidiConvert.load("assets/0/cda070.mid", function(midi) {
+		
+	  // make sure you set the tempo before you schedule the events
+	  Tone.Transport.bpm.value = midi.header.bpm
+	  
+	  // pass in the note events from one of the tracks as the second argument to Tone.Part 
+	  var midiPart = new Tone.Part(function(time, note) {
+
+	    //use the events to play the synth
+	    piano.triggerAttackRelease(note.name, note.duration, time, note.velocity)
+
+	  }, midi.tracks[0].notes).start()
+
+	  // start the transport to hear the events
+	  Tone.Transport.start()
+	})
+}
+function playMP3(){
 	MidiConvert.load("assets/0/cda070.mid", function(midi) {
 		
 	  // make sure you set the tempo before you schedule the events
